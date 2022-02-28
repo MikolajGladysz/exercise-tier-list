994f4be6950c7a85095d2cd44e1c82f36938c562
MikolajGladysz <mikolajgladysz01@gmail.com>
2022-02-28 21:03:55 +0100

    refactoring muscle view

--- js/controller.js
+++ js/controller.js
@@ -4,14 +4,13 @@ import muscleView from "./views/muscleView.js";
 
 const controlMuscles = function () {
   const id = window.location.hash.slice(1);
-
   if (!id) return;
 
-  //0) update muscle preview view
+  //   muscleView.highlightMuscle(id);
+  muscleView.highlightMuscle(id);
 };
 
 const init = function () {
-  controlMuscles();
-  muscleView.hideMuscleParts();
+  muscleView.addHandler(controlMuscles);
 };
 init();
--- js/views/muscleView.js
+++ js/views/muscleView.js
@@ -5,27 +5,51 @@ class MuscleView extends View {
   _errorMessage = "Could not load an image";
   _message = "";
 
-  hideMuscleParts() {
-    document.body.addEventListener("click", function (e) {
-      const musclePart = e.target.closest(".mscl-img > a")?.parentElement;
-      const muscleFront = document.querySelector(".mscl-front");
-      const muscleBack = document.querySelector(".mscl-back");
-
-      if (
-        muscleBack.classList.contains("hidden") ||
-        muscleFront.classList.contains("hidden")
-      ) {
-        if (e.target.parentElement !== musclePart) {
-          muscleBack.classList.remove("hidden");
-          muscleFront.classList.remove("hidden");
-        }
+  highlightMuscle(id) {
+    // 1) Get all muscle images
+    const collection = document.querySelectorAll(".muscle");
+    // 2) Check which muscle matech hash and setting it to active
+    collection.forEach((el) => {
+      el.classList.remove("active");
+      if (el.alt.toLowerCase() === id) {
+        el.classList.add("active");
       }
-
-      if (!musclePart) return;
-      if (musclePart.classList.contains("mscl-front"))
-        muscleBack.classList.add("hidden");
-      else muscleFront.classList.add("hidden");
     });
+  }
+
+  addHandler(handler) {
+    // document.body.addEventListener("click", handler)
+    window.addEventListener("load", handler);
+    window.addEventListener("hashchange", handler);
+
+    document.body.addEventListener("click", this._hideMuscleParts.bind(this));
+  }
+
+  _hideMuscleParts(e) {
+    if (!e) return;
+    const musclePart = e.target.closest(".mscl-img > a")?.parentElement;
+    const muscleFront = document.querySelector(".mscl-front");
+    const muscleBack = document.querySelector(".mscl-back");
+
+    // 0) check if click is outside of image, to reset view
+    if (
+      muscleBack.classList.contains("hidden") ||
+      muscleFront.classList.contains("hidden")
+    ) {
+      if (e.target.parentElement !== musclePart) {
+        muscleBack.classList.remove("hidden");
+        muscleFront.classList.remove("hidden");
+        window.location.hash = "";
+        this.highlightMuscle("");
+      }
+    }
+
+    if (!musclePart) return;
+
+    // 1) Hidding muscle part, to make room for muslce preview window
+    if (musclePart.classList.contains("mscl-front"))
+      muscleBack.classList.add("hidden");
+    else muscleFront.classList.add("hidden");
   }
 }
 export default new MuscleView();
